c8c949716b82d3c97b537c1f4f4b276cc15cd94c
a6santa <anderson.santana@beblue.com.br>
2019-06-30 17:15:07 -0300

    [feature/gspreadsheet] add new method in class GSpreadSheet

--- src/etl/connection/GSpreadSheet.py
+++ src/etl/connection/GSpreadSheet.py
@@ -38,12 +38,20 @@ class GSpreadSheet(object):
         """
         return pd.DataFrame(self.conn.open_by_key(id_spreadsheet).worksheet(name_worksheet).get_all_records())
 
+    def get_spreadsheet(self, id_spreadsheet=None, name_worksheet=None):
+        """
+            id_spreadsheet: id da planilha do google
+                EX:.
+                    '1Scm62YFz8s6DL2INsaW6otiPxlYQI_9imxFANvQwYLc'
+            name_worksheet: nome da pasta de trabalho da planilha selecionada
+        """
+        return self.conn.open_by_key(id_spreadsheet).worksheet(name_worksheet).get_all_records()
+
 if __name__ == '__main__':
     import os
     import etl.util.reader_json as rj
     name_conn = 'google'
     auth = os.environ.get(rj.reader_json(path='src/etl/config/auth_conn.json')[name_conn]['file_path'])
     G = GSpreadSheet(auth)
-    df = G.get_spreadsheet_to_pandas('1J8jA-atpyIpH47gEMUmlGdcrxj6dIsUGRtLbeVSlKOE', 'sales')
-    print(df.head(50))
-    print(df.dtypes)
+    df = G.get_spreadsheet('1J8jA-atpyIpH47gEMUmlGdcrxj6dIsUGRtLbeVSlKOE', 'sales')
+    print(df)
